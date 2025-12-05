a8bfc058af517af4c523100690a543701fb3754a
TurahRae <Turahrae@gmail.com>
2025-12-05 13:20:43 +0100

    Initial commit - React Pos Dashboard setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Turah Rae POS Dashboard</h1>
+      <p>App is ready! </p>
+    </div>
+  );
+}
+
+export default App;
